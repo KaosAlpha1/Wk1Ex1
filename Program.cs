812c52d6ef17a867d7d025f172ac27edab9601de
Michael Hegwood <54250433+KaosAlpha1@users.noreply.github.com>
2025-01-16 21:45:08 -0600

    excersise 2
    excersise 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Week_1_work
+namespace ConsoleApp1
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to ITSE1302");
+            Console.Write("What is your first name? ");
+            string Fname = Console.ReadLine();
+            Console.Write("What is your last name? ");
+            string Lname = Console.ReadLine();
+            Console.Write("What is your age? ");
+            string age = Console.ReadLine();
+            Console.Write("What is your favorite color? ");
+            string color = Console.ReadLine();
+            Console.Write("Hello, "+Fname +" " + Lname +"!" + " You are " + age + " years old and your favorite color is " + color);
         }
     }
 }
