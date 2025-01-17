89a41809db423d18e511846fb5bbc20a04d94594
Michael Scott <michaelscott.msh@gmail.com>
2025-01-16 20:37:23 -0600

    Excersise 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week_1_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to ITSE1302");
+        }
+    }
+}
